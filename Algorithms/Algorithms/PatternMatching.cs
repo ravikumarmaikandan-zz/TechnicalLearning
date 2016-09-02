f01c0f9ab8180a39dc8be2c15e7a658e07e3eb6f
ravikumarmaikandan <ravi.maikandan@gmail.com>
2016-09-02 08:38:43 -0400

    FIRST COMMIT

---
+++ Algorithms/Algorithms/PatternMatching.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public static class Patternmatching
+{
+
+  public static void KMPAlgorithm()
+  {
+    //Knuth Morris Pratt pattern searching algorithm.
+    //Step 1 Generate the LPS array  (LPS indicates longest proper prefix which is also a suffix)
+    //Step 2 Use the LPS table to perform the pattern matching 
+    var inputData = KMPInputData();
+    var pattern = inputData.Item1;
+    var actualString = inputData.Item2;
+
+    //Compute LPS array
+    int[] lps = new int[pattern.Length];
+    ComputeLPSArray(pattern, actualString.Length, lps);
+  }
+
+  private static Tuple<string,string> KMPInputData()
+  {
+    var pattern = "ABABCABAB";
+    var actualString = "ABABDABACDABABCABAB";
+    return Tuple.Create(pattern, actualString);
+  }
+  private static void ComputeLPSArray(string pattern, int lengthOfString, int[] lps)
+  {
+    var length = 0;
+    lps[0] = 0;
+    int i = 1;
+    while(i < lengthOfString)
+    {
+      if(pattern.ToCharArray[i] == pattern.ToCharArray[length])
+      {
+
+      }
+    }
+  }
+}
+
